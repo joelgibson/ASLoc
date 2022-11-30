8e8878569ef2cd51e854aec42ee22cf543744bfb
geordw <58535462+geordw@users.noreply.github.com>
2022-12-01 08:20:15 +1100

    added Zam A2

---
+++ Tests/ZamolodchikovA2.m
@@ -0,0 +1,36 @@
+// This test verifies the A3 Zamolodchikov relation, as written on page g0 of Soergel Calculus by Elias and Williamson.
+if assigned batch then SetQuitOnError(true); else SetDebugOnError(true); end if;
+
+AttachSpec("ASLoc.spec");
+SetColumns(0);
+SetAssertions(3);
+
+// A3 relation, as written on page 10.
+// You really need the picture to make sense of the following code. R, G, B stand for Red, Green, and Blue.
+cartan := CartanMatrix("A3");
+W := CoxeterGroup(GrpFPCox, cartan);
+B := BSCategory(cartan, W);
+
+g := 1;
+r := 2;
+b := 3;
+
+LHS := &*[
+    Braid(B, r, g) cat [b, r, g],
+    [r, g] cat Braid(B, b, r) cat [g],
+    [r] cat Braid(B, b, g) cat [r] cat Braid(B, g, b),
+    [r, b] cat Braid(B, r, g) cat [b],
+    Braid(B, b, r) cat [g, r, b],
+    [b, r] cat Braid(B, g, b) cat [r, b]
+];
+RHS :=&*[
+    [g, r] cat Braid(B, b, g) cat [r, g],
+    [g, r, b] cat Braid(B, r, g),
+    [g] cat Braid(B, b, r) cat [g, r],
+    Braid(B, b, g) cat [r] cat Braid(B, g, b) cat [r],
+    [b] cat Braid(B, r, g) cat [b, r],
+    [b, r, g] cat Braid(B, b, r)
+];
+assert LHS eq RHS;
+
+quit;
